7241651ac84a870204e9c2fb23ecfa9f0b9dc348
Rahmat Nur Isni <rahmatisni@gmail.com>
2022-07-26 14:29:14 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('role')->default('USER')->comment('USER, ADMIN');
             $table->integer('tenant_id')->unsigned()->nullable();
+            $table->integer('business_id')->unsigned()->nullable();
+            $table->integer('rest_area_id')->unsigned()->nullable();
+            $table->integer('paystation_id')->unsigned()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
