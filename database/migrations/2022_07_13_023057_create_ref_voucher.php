f2ff6e566f0e27356a60fb9ab00703a800bcb6a1
Rahmat Nur Isni <rahmatisni@gmail.com>
2022-07-26 14:03:58 +0700

    Update 2022_07_13_023057_create_ref_voucher.php

--- database/migrations/2022_07_13_023057_create_ref_voucher.php
+++ database/migrations/2022_07_13_023057_create_ref_voucher.php
@@ -19,14 +19,16 @@ class CreateRefVoucher extends Migration
             $table->string('customer_id');
             $table->string('phone')->nullable();
             //$table->integer('trx_id')->unsigned();
-            $table->string('balance')->nullable();
-            $table->string('balance_history')->nullable();
-            $table->string('qr')->nullable();
+            $table->string('username');
+            $table->string('balance');
+            $table->string('balance_history');
+            $table->string('qr_code_use');
             $table->string('auth_id');
             $table->integer('paystation_id')->unsigned()->nullable();
-
             $table->integer('created_by')->nullable();
             $table->timestamps();
+            $table->integer('is_active');
+
         });
 
         Schema::create('ref_voucher_detail', function (Blueprint $table) {
