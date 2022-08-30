07b86518dcff61def5b3e580a5272a862d2fc73a
Asep <asep.nasihin@yahoo.com>
2022-08-30 17:49:45 +0700

    chat user

--- app/Http/Controllers/API/ChatController.php
+++ app/Http/Controllers/API/ChatController.php
@@ -31,8 +31,8 @@ class ChatController extends Controller
     public function store(ChatRequest $request)
     {
         $chat = [
-            'user_id' => auth()->user()->id ?? 0,
-            'user_name' => auth()->user()->name ?? '',
+            'user_id' => auth()->user()->id ?? $request->user_id,
+            'user_name' => auth()->user()->name ?? $request->user_name,
             'text'  => $request->text ?? '-',
             'date' => date('Y-m-d H:i:s'),
         ];
