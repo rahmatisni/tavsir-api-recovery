5276996d9a721fcfef02eea294e2f0a369b90275
Asep <asep.nasihin@yahoo.com>
2023-01-05 10:07:17 +0700

    fix

--- database/migrations/2022_07_12_020502_create_trans_order.php
+++ database/migrations/2022_07_12_020502_create_trans_order.php
@@ -18,7 +18,6 @@ class CreateTransOrder extends Migration
             $table->string('order_id');
             $table->string('consume_type')->comment('dine_in or take_away')->nullable();
             $table->string('nomor_name')->comment('nomor meja atau nama customer')->nullable();
-            $table->string('consume_name');
             $table->integer('sub_total')->unsigned();
             $table->integer('fee')->unsigned();
             $table->integer('service_fee')->unsigned();
