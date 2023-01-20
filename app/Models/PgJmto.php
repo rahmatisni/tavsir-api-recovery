05671b9fc3bb317b75aa15087a29f2d208ddf0c1
Asep <asep.nasihin@yahoo.com>
2023-01-20 11:04:54 +0700

    fix: update sof bri va

--- app/Models/PgJmto.php
+++ app/Models/PgJmto.php
@@ -255,7 +255,7 @@ class PgJmto extends Model
 
     public static function feeBriVa()
     {
-        return PgJmto::tarifFee(1, 2, null);
+        return PgJmto::tarifFee(254, 2, null);
     }
 
     public static function feeMandiriVa()
