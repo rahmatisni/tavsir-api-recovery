a1e4eae6d24eb7855f6b00051b18b1621bf306c5
Asep <asep.nasihin@yahoo.com>
2023-02-21 13:58:27 +0700

    test: kios bank

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -6,57 +6,12 @@ use Illuminate\Support\Facades\Http;
 
 class KiosBankService
 {
-    function post($url,$header = '',$params=false){
-        $curl=curl_init();
-    
-        if($params===false)
-            $query='';
-        else 
-            $query=json_encode($params);
-    
-        curl_setopt_array($curl,array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_TIMEOUT => 800,
-            CURLOPT_HEADER=>true,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $query,
-            CURLOPT_HTTPHEADER => array(
-                $header,
-                'content-type:application/json'
-            ),
-            CURLOPT_SSL_VERIFYHOST=>0,
-            CURLOPT_SSL_VERIFYPEER=>0
-        ));
-        $response=curl_exec($curl);
-        $err=curl_error($curl);
-        curl_close($curl);
-        if($err){
-            return $err;
-        } else {
-            return $response;
-        }
-    }
-
-    public function auth($id)
+    public function auth()
     {
-        switch ($id) {
-            case 1:
-                $res = $this->post(url: env('KIOSBANK_URL'));
-                return $res;
-            
-            case 2:
-                $res = Http::withOptions([
-                    'verify' => false,
-                ])->get(env('KIOSBANK_URL'));
-                return $res->headers();
+        $res = Http::withOptions(['verify' => false,])->get(env('KIOSBANK_URL'));
+        $diges = $res->header('WWW-Authenticate');
 
-                break;
-            
-            default:
-                # code...
-                break;
-        }
+        return $diges;
     }
 
 }
