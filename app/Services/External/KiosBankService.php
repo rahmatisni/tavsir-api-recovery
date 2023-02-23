594d23247b1b4a1e351796e62e60a1cb1238749a
Asep <asep.nasihin@yahoo.com>
2023-02-23 15:30:34 +0700

    feat: single payment kiosbnak

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -340,7 +340,7 @@ class KiosBankService
             'tagihan'=>$sub_total,
             'sessionID'=> $this->getSeesionId(),
             'productID'=>$order[0],
-            'referenceID'=>Carbon::now()->format('YmdHis'),
+            'referenceID'=>'900000000004',
             'merchantID'=>env('KIOSBANK_MERCHANT_ID'),
             'customerID'=>$order[1]
         );
