d5ca2dc01f3f8de09e4017877fd7689888e9cc6c
Asep <asep.nasihin@yahoo.com>
2023-02-27 15:31:11 +0700

    test: kiosbank

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -38,6 +38,7 @@ class TsOrderResource extends JsonResource
             'casheer_name' => $this->casheer->name ?? '',
             'total_pesanan' => $this->detil->count(),
             'rating' => $this->rating,
+            'description' => $this->description,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'payment' => $this->payment->data ?? null,
             'detil' => TsOrderDetilResource::collection($this->detil),
