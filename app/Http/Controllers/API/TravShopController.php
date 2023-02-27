c3beea9da349b217555a34dc7468032416e70f88
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-02-27 08:29:53 +0700

    dd cek

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -680,7 +680,7 @@ class TravShopController extends Controller
                     $data->save();
                     if($data->order_type == TransOrder::ORDER_TRAVOY){
                         $kios = $this->kiosBankService->singlePayment($data->sub_total, $data->order_id);
-                        
+                        dd($kios);
                         if($kios['rc'] == '00'){
                             $data->status = TransOrder::DONE;
                             $data->save();
