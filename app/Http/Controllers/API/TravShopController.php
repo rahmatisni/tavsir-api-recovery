5822bf71239636cc5f3b44ee43b35bb7767fb53d
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-03 13:45:52 +0700

    Feat:[add barrier model-orderpulsa]->final

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -706,9 +706,12 @@ class TravShopController extends Controller
                         ]);
                         if($kios['rc'] == '00'){
                             $data->status = TransOrder::DONE;
+                            $data->save();
                             return response()->json(['status' => $data->status, 'responseData' => $data->payment->data ?? '', 'kiosbank' => $kios]);
                         }
                         else {
+                            $data->status = TransOrder::PAYMENT_SUCCESS;
+                            $data->save();
                             return response()->json(['status' => $data->status, 'responseData' => $data->payment->data ?? '', 'kiosbank' => $kios]);
 
                         }
