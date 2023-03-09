af410693dd52c2c01523b6d04fcfda32c5818995
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-09 08:42:04 +0700

    add kiosbank resp on order non pulsa

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -346,25 +346,16 @@ class KiosBankService
                 $order->sub_total = $res_json['data']['total'];
                 $order->total = $res_json['data']['total'];
                 $order->description = $res_json['data']['noHandphone'] ?? $res_json['data']['idPelanggan'].'-'.$res_json['productID'].'-'.$res_json['data']['nama'];
-                $order->kiosbank = $res_json['data'];
-                $order->log_kiosbank()->updateOrCreate(['trans_order_id' => new TransOrder()],[
-                    'data' => $res_json['data']
-                ]);
                 $order->save();
-                DB::commit();
-
+                $order->kiosbank = $res_json['data'];
                 return $order;
 
             }
             else {
                 $order->sub_total = $res_json['data']['harga'] ?? $res_json['data']['total'] ?? $res_json['data']['totalBayar'] ?? $res_json['data']['tagihan'];
                 $order->description = $res_json['data']['noHandphone'] ?? $res_json['data']['idPelanggan'].'-'.$res_json['productID'].'-'.$res_json['data']['nama'];
-                $order->kiosbank = $res_json['data'];
-                $order->log_kiosbank()->updateOrCreate(['trans_order_id' => new TransOrder()],[
-                    'data' => $res_json['data']
-                ]);
                 $order->save();
-                DB::commit();
+                $order->kiosbank = $res_json['data'];
                 return $order;
             }
           
