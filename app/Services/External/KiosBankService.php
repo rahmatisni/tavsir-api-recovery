a5cb8540ad7b886342f40257a9256a54ef2d578c
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-13 10:09:15 +0700

    log digest

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -309,7 +309,7 @@ class KiosBankService
                 // dd($data);
                 $request['data']['idPelanggan'] = $request['data']['noHandphone'] ?? $request['data']['idPelanggan'] ?? '-';
                 $request['data']['noReferensi'] = $request['referenceID'] ?? $request['data']['noReferensi'] ?? '-';
-                dd($request);
+                dd($data->id);
                 $data->log_kiosbank()->updateOrCreate(['trans_order_id' => $data->id],[
                     $request
                 ]);              
