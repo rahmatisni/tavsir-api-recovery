37819a3d7576a06331be5bf47b387ef0e10f7d55
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-14 22:14:21 +0700

    feat:kios->update inquiry log

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -382,7 +382,7 @@ class KiosBankService
         ];
         $res_json =  $this->http('POST',self::INQUIRY,$payload);
         $res_json = $res_json->json();
-        Log::info($payload, $res_json);
+        Log::info($payload+'\n\n'+$res_json);
 
         if($res_json['rc'] == '00')
         {
