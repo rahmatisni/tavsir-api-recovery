a04aaeb86e208793fa66c142e0cefd874a5722be
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-14 23:48:25 +0700

    feat:kios -> order pulsa param

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -255,7 +255,7 @@ class KiosBankService
             ],
             'description'=>'INQUIRY'
         ];
-        $this->callback($request->json());
+        $this->callback($request);
 
         return $order;
     }
