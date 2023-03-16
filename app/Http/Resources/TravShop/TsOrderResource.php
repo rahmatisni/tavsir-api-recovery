1fd0bfd2b7dec90f2c3f775acfb1b1675de2107c
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-16 12:29:17 +0700

    regex kios log

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -85,7 +85,7 @@ class TsOrderResource extends JsonResource
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'payment' => $this->payment->data ?? null,
             // 'log_kiosbank' => $this->log_kiosbank,
-            'log_kiosbank' => $temps ?? $this->log_kiosbank->data,
+            'log_kiosbank' => $temps ?? $this->log_kiosbank,
             'detil' => TsOrderDetilResource::collection($this->detil),
             'detil_kios' => $product_kios
         ];
