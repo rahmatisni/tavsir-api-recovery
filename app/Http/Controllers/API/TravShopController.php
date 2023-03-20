5678087e48b7e4cc0b74c138f6d34c243bcb761b
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-20 08:20:48 +0700

    UAT

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -801,8 +801,7 @@ class TravShopController extends Controller
                         $kios['data']['harga'] = $kios['data']['harga'] ?? ($data->sub_total ?? '0');
                         // $kios['data']['nama'] = $kios['data']['nama'] ?? $datalog['data']['data']['nama'] ?? '-';
                         // $kios['data']['nominalProduk'] = $kios['data']['nominalProduk'] ?? $datalog['data']['data']['nominalProduk'] ?? '0';
-                        $kios['description'] = $kios['description'] ?? $kios['data']['status'] ?? '-';
-
+                        $kios['description'] = $kios['description'] ?? $kios['data']['status'] ?? $kios['data']['description'] ?? '';
                         
                         if($kios['rc'] == '00')
                         {
