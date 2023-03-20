1f7e60caae933b68223b0639e0375b9cd537abff
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-20 15:02:39 +0700

    fix

--- app/Http/Controllers/API/KiosBank/KiosBankController.php
+++ app/Http/Controllers/API/KiosBank/KiosBankController.php
@@ -51,7 +51,7 @@ class KiosBankController extends Controller
             return response()->json(['message' => 'Nomor Tidak Sesuai Dengan Produk Yang Dipilih!'], 422);
         }
         $datax = $this->service->getProduct($kategori_pulsa);
-        $validator = json_decode($datax['PULSA']->kode);
+        $validator = json_decode($datax['PULSA']);
         dd($validator);
         $data = $this->service->orderPulsa($reqest->validated(), $validator);
         return response()->json($data);
