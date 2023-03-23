e73648cdf5186730d6629610fab0ca43d29c90ea
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-23 12:53:04 +0700

    fix amount harga

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -55,15 +55,20 @@ class TsOrderResource extends JsonResource
                 $temps = $this->log_kiosbank->data;
                 $temps['data'] = [];
                 // $slice = ['harga_kios'];
-                $param = ['Admin_Bank','Total', 'Jumlah_Pembayaran','Harga_kios'];
+                $param = ['Admin_Bank','Total', 'Jumlah_Pembayaran'];
+                $slice = ['Harga_kios'];
                 // dd($temps);
                 foreach($temp as $key => $val) {
                     $key = ucwords(preg_replace("/(?<=[a-zA-Z])(?=[A-Z])/", "_", $key));
                    
                     if (in_array($key, $param))
                     {
-                        $temps['data'][$key] = 1;
-                        // $temps['data'][$key] = rupiah((int)$val);
+                        // $temps['data'][$key] = 1;
+                        $temps['data'][$key] = rupiah((int)$val);
+
+                    }
+                    elseif (in_array($key, $slice)){
+                        $temps['data'][$key] =1;
 
                     }
                     else {
