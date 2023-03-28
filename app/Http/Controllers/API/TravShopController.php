b1ca8637a296ec75c16ca30b69cd980678603100
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-03-28 16:45:23 +0700

    log tarif

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -340,6 +340,7 @@ class TravShopController extends Controller
                     if($tenant_is_verified || $trans_order->order_type == TransOrder::ORDER_TRAVOY)
                     {
                         $data = PgJmto::tarifFee($value->sof_id,$value->payment_method_id, $value->sub_merchant_id,$trans_order->sub_total);
+                        log::info($data);
                         $value->fee = $data;
                     }else{
                         $removes[] = $value->id;
