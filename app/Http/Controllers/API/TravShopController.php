fa794c3d7ed7025503eceabde1619184e0e8a8c3
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-04-02 22:11:58 +0700

    add merchant log list

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -683,7 +683,6 @@ class TravShopController extends Controller
 
                     $kios = $this->kiosBankService->cekStatus($data->sub_total, $data->order_id, $adminBank, $data->harga_kios);
                     Log::info($kios);
-                    dd($kios);
                     $kios['data']['harga_kios'] = $data->harga_kios;
                     $kios['data']['harga'] = $data->sub_total ?? '0';
                     $kios['description'] = $kios['description'] ?? $kios['data']['status'] ?? $kios['data']['description'] ?? '-';
