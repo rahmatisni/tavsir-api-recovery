e9bffafccab471eaa4ae0de1ae5b396199402759
Asep <asep.nasihin@yahoo.com>
2023-04-03 12:44:35 +0700

    fix

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -670,10 +670,109 @@ class TravShopController extends Controller
         return response()->json(new TsPaymentresource($data->payment));
     }
 
+    function post($url,$header,$params=false){
+        $curl=curl_init();
+    
+        if($params===false)
+            $query='';
+        else 
+            $query=json_encode($params);
+    
+        curl_setopt_array($curl,array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 800,
+            CURLOPT_HEADER=>true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $query,
+            CURLOPT_HTTPHEADER => array(
+                $header,
+                'content-type:application/json'
+            ),
+            CURLOPT_SSL_VERIFYHOST=>0,
+            CURLOPT_SSL_VERIFYPEER=>0
+        ));
+        $response=curl_exec($curl);
+        $err=curl_error($curl);
+        curl_close($curl);
+        if($err){
+            return $err;
+        } else {
+            return $response;
+        }
+    }
+        
+    function auth_response($params,$uri,$request_method){
+        /*
+            SESUAIKAN INI
+        */
+        $username='dji';
+        $password='abcde';
+        $nc='1';//berurutan 1,2,3..dst sesuai request
+        $cnonce=uniqid();
+    
+        $a1=md5($username.':'.$params['Digest realm'].':'.$password);
+        $a2=md5($request_method.':'.$uri);
+        $response=md5($a1.':'.$params['nonce'].':'.$nc.':'.$cnonce.':'.$params['qop'].':'.$a2);
+        $query=array(
+            'username'=>$username,
+            'password'=>$password,
+            'realm'=>$params['Digest realm'],
+            'nonce'=>$params['nonce'],
+            'uri'=>$uri,
+            'qop'=>$params['qop'],
+            'nc'=>$nc,
+            'cnonce'=>$cnonce,
+            'opaque'=>$params['opaque'],
+            'response'=>$response
+        );
+        $query_str='username="'.$query['username'].'",realm="'.$query['realm'].'",nonce="'.$query['nonce'].'",uri="'.$query['uri'].'",qop="'.$query['qop'].'",nc="'.$query['nc'].'",cnonce="'.$query['cnonce'].'",response="'.$query['response'].'",opaque="'.$query['opaque'].'"';
+        return $query_str;
+    }
+
+    function cek ()
+    {
+         /*
+            SESUAIKAN IP DAN PORT
+        */
+        $ip_interface='10.11.12.5';
+        $port_interface='16551';
+        
+        $full_url='https://'.$ip_interface.':'.$port_interface.'/Services/getCurrentDeposit';
+        $sign_on_response=$this->post($full_url,'');
+        $response_arr=explode('WWW-Authenticate: ', $sign_on_response);
+        
+        $response_arr_1=explode('error', $response_arr[1]);
+        $response=trim($response_arr_1[0]);
+        $auth_arr=explode(',',$response);
+        $auth_sorted=array();
+        foreach($auth_arr as $auth){
+            list($key,$val)=explode('=', $auth);
+            $auth_sorted[$key]=substr($val, 1, strlen($val)-2);
+        }
+        $auth_query=$this->auth_response($auth_sorted,'/Services/getCurrentDeposit','POST');
+        
+        $post_header='Authorization : Digest '.$auth_query;
+        /*
+             SESUAIKAN INI
+        */
+        $body_params=array(
+            'sessionID'=>'58AF525549A33026E1F8E502FEE72721',
+            'mitra'=>'DJI',
+            'accountID'=>'ujicoba',
+            'merchantID'=>'DJI000343',
+            'counterID'=>'1'
+        );
+        $post_response=$this->post($full_url,$post_header,$body_params);
+        echo '<pre>'.$post_response.'</pre>';
+    }
+
     public function statusPayment(Request $request, $id)
     {
-        $asd = $this->kiosBankService->cekDeposit();
-        dd($asd);
+        
+       
+
+        dd($this->cek());
 
         $data = TransOrder::with('payment_method')->findOrfail($id);
         try {
