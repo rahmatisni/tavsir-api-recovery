8484ca59bc014e535282bb48a01ab494dcea0e74
Asep <asep.nasihin@yahoo.com>
2023-04-03 12:59:59 +0700

    fix

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -770,8 +770,7 @@ class TravShopController extends Controller
 
     public function statusPayment(Request $request, $id)
     {
-        dd('cek');
-       $this->kiosBankService->cekDeposit();
+       dd($this->kiosBankService->cekDeposit());
 
         dd($this->cek());
 
