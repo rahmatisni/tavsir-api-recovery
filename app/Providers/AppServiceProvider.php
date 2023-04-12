0590d706763e3e4fad87f43ac6728c85f1cc0486
Asep <asep.nasihin@yahoo.com>
2023-04-12 07:40:26 +0700

    fix: auth log viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +35,23 @@ class AppServiceProvider extends ServiceProvider
         Http::macro('kiosbank', function () {
             return Http::baseUrl(env('KIOSBANK_URL'))->withOptions(["verify"=>false]);
         });
+        
+        if(env('LOG_VIEWER_AUTH') === true)
+        {
+            LogViewer::auth(function ($request) {
+                $auth = $request->header('authorization');
+                if($auth)
+                {
+                    $token = explode(' ',$auth);
+                    $token = $token[1] ?? '';
+                    $basic = base64_decode($token);
+                    $basic = explode(':',$basic);
+                    $email = $basic[0] ?? '';
+                    $user_role = User::where('email', $email)->first()?->role;
+                    return ($user_role == User::ADMIN);
+                }
+                return false;
+            });
+        }
     }
 }
