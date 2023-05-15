226600959ade12862ea17644998c01f3074974f0
Asep <asep.nasihin@yahoo.com>
2023-05-15 10:23:59 +0700

    fix

--- app/Http/Controllers/API/PaymentGatewayController.php
+++ app/Http/Controllers/API/PaymentGatewayController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\PgJmto;
-use Illuminate\Http\Request;
 
 class PaymentGatewayController extends Controller
 {
