3cb7a16130750f6dafe9ba860c965f2ad45ae723
rahmatisni <rahmatisni@gmail.com>
2023-06-16 10:33:18 +0700

    devel: PLN

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1141,7 +1141,7 @@ class TravShopController extends Controller
                         }
                         if ($data->description == 'dual') {
                             $datalog = $data->log_kiosbank()->where('trans_order_id', $id)->first();
-                            $tagihan = $datalog['data']['data']['tagihan'] ??  $datalog['data']['data']['tf'] ?? $datalog['data']['data']['harga_kios'];
+                            $tagihan = $datalog['data']['data']['tagihan'] ??  $datalog['data']['data']['TF'] ?? $datalog['data']['data']['harga_kios'];
                             $admin = $datalog['data']['data']['adminBank'] ?? $datalog['data']['data']['AB'] ?? '000000000000';
                             $total = $datalog['data']['data']['total'] ?? $datalog['data']['data']['harga_kios'] ?? $tagihan;
                             $kios = $this->kiosBankService->dualPayment($data->sub_total, $data->order_id, $tagihan, $admin, $total);
