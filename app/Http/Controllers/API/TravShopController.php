b3408ca12406a2ba9c874d838caee7b98b08f8bd
rahmatisni <rahmatisni@gmail.com>
2023-06-21 15:37:05 +0700

    cr:penyesuaian percentage pg

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -399,9 +399,11 @@ class TravShopController extends Controller
                     if ($tenant_is_verified || $trans_order->order_type == TransOrder::ORDER_TRAVOY) {
                         $data = PgJmto::tarifFee($value->sof_id, $value->payment_method_id, $value->sub_merchant_id, $trans_order->sub_total);
                         // log::info($data);
-                        
 
-                        if ($data['is_presentage'] == (false || null))
+                        $value->percentage = $data['is_presentage'] ?? null;
+
+
+                        if ($value->percentage == false || null)
                         {
                             $value->fee = $data['value'] ?? null;
                         }
