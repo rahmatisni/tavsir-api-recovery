2c12ac854b7206dbabc75c997eee1011b3658bca
rahmatisni <rahmatisni@gmail.com>
2023-07-04 15:06:20 +0700

    sesuain pln

--- app/Services/External/KiosBankService.php
+++ app/Services/External/KiosBankService.php
@@ -498,7 +498,8 @@ class KiosBankService
             'merchantID' => env('KIOSBANK_MERCHANT_ID'),
             'productID' => $ref[0],
             'customerID' => $data['phone'],
-            'referenceID' => $ref[2],
+            'referenceID' => $ref[2]
+            // 'productIDPLN' => $data['code']
         ];
         $current_date_time = Carbon::now()->toDateTimeString();
         // if ((substr($data['code'],  0, 3) == '753') && (strlen($data['phone']) != 16)) {
@@ -561,7 +562,7 @@ class KiosBankService
                 // $disc = 2500;
                 $disc = 0;
 
-                $harga_jual_kios = ProductKiosBank::where('kode', $res_json['productID'])->first() ?? $res_json['data']['total'];
+                $harga_jual_kios = ProductKiosBank::where('kode', $res_json['productID'])->first() ??  ProductKiosBank::where('kode', $data['code'])->first() ?? $res_json['data']['total'];
                 $temp_harga = preg_replace('/[^0-9]/', '', $harga_jual_kios['name']) + $res_json['data']['AB'];
                 
                 $order->harga_kios = $temp_harga;
