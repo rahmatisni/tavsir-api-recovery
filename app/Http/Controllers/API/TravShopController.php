261bf5d7845935ea57e9ea981baf7c01183113fd
rahmatisni <rahmatisni@gmail.com>
2023-07-11 11:48:42 +0700

    fix_order_confirmation_tavsir

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -619,7 +619,7 @@ class TravShopController extends Controller
                         $request->customer_phone,
                         $request->customer_email,
                         $request->customer_name,
-                        $data->tenant?->sub_merchant_id ?? $data->sub_merchant_id,
+                        $data->tenant?->sub_merchant_id ?? $data->sub_merchant_id
                     );
                     if ($res['status'] == 'success') {
                         $pay = null;
