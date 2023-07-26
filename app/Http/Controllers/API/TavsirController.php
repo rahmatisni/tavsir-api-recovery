987500f69a5db250a0a8131fbfbb5a90d86159c4
rahmatisni <rahmatisni@gmail.com>
2023-07-26 13:28:49 +0700

    feat(TAVSIR):Order List Sort By

--- app/Http/Controllers/API/TavsirController.php
+++ app/Http/Controllers/API/TavsirController.php
@@ -726,7 +726,7 @@ class TavsirController extends Controller
             $data = $data->orderBy('updated_at', 'desc');
 
         }
-        $data = $data->orderByRaw("FIELD(status , 'WAITING_OPEN', 'READY',  'PAYMENT_SUCCESS', 'DONE', 'CANCEL') DESC")->get();
+        $data = $data->orderByRaw("FIELD(status , 'WAITING_OPEN', 'READY',  'PAYMENT_SUCCESS', 'DONE', 'CANCEL') ASC")->get();
         return response()->json(TrOrderResource::collection($data));
     }
 
