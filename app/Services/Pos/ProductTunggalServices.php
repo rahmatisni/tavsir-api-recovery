57d3f22e39b42084e00dd89f62645e48be867dfd
Asep <asep.nasihin@yahoo.com>
2023-08-15 11:39:23 +0700

    fix: product composit harga min max

--- app/Services/Pos/ProductTunggalServices.php
+++ app/Services/Pos/ProductTunggalServices.php
@@ -29,8 +29,25 @@ class ProductTunggalServices
             $data->tenant_id = auth()->user()->tenant_id;
             $data->fill($paylod);
             $data->price_capital = $data->price_capital;
-            $data->price_min = $data->price_capital;
-            $data->price_max = $data->price_capital;
+            if($paylod['is_composit'] == 0)
+            {
+                $data->price_min = $data->price_capital;
+                $data->price_max = $data->price_capital;
+            }
+
+            if($paylod['is_composit'] == 1)
+            {
+                $product_min = 0;
+                $product_max = 0;
+                foreach ($paylod['raw'] as $value) {
+                    //Product sudah divalidasi di request
+                    $product = Product::find($value['child_id']);
+                    $product_min += ($product->price_min * $value['child_id']);
+                    $product_max += ($product->price_max * $value['child_id']);
+                }
+                $data->price_min = $data->product_min;
+                $data->price_max = $data->product_max;
+            }
             $data->save();
             if($paylod['is_composit'] == 0){
                 $data->trans_stock()->create([
