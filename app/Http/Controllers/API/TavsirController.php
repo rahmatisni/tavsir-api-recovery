b86e15675114d2f4c6e26450b09e5fd149becf63
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-13 22:40:42 +0700

    fix status()

--- app/Http/Controllers/API/TavsirController.php
+++ app/Http/Controllers/API/TavsirController.php
@@ -755,22 +755,29 @@ class TavsirController extends Controller
                 //     $value->tavsir = false;
                 // }
 
-                if ($value->sof_id) {
+                if ($value?->sof_id) {
                     // tenant_is_verified
                     // if ($tenant_is_verified || $trans_order->order_type == TransOrder::ORDER_TRAVOY) {
 
-                    $data = PgJmto::tarifFee($value->sof_id, $value->payment_method_id, $value->sub_merchant_id, $trans_order->sub_total);
-                    Log::warning($data);
-                    $value->percentage = $data['is_presentage'] ?? null;
-                    $x = $data['value'] ?? 'x';
-                    $state = $data['is_presentage'] ?? null;
-
-
-                    if ($state == (false || null)) {
-                        $value->fee = $data['value'] ?? null;
-                    } else {
-                        $value->fee = (int) ceil((float) $x / 100 * $trans_order->sub_total);
+                    if($value?->sof_id == null)
+                    {
+                        $value->percentage = null;
+                        $value->fee = null;
                     }
+                    else {
+                        $data = PgJmto::tarifFee($value->sof_id, $value->payment_method_id, $value->sub_merchant_id, $trans_order->sub_total);
+                        Log::warning($data);
+                        $value->percentage = $data['is_presentage'] ?? null;
+                        $x = $data['value'] ?? 'x';
+                        $state = $data['is_presentage'] ?? null;
+    
+    
+                        if ($state == (false || null)) {
+                            $value->fee = $data['value'] ?? null;
+                        } else {
+                            $value->fee = (int) ceil((float) $x / 100 * $trans_order->sub_total);
+                        }
+                    } 
                 }
             }
 
