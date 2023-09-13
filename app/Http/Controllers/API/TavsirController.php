bf33e1aa4d4b0ee7e9224d5187b825c6f1cb0371
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-13 18:13:34 +0700

    fix status()

--- app/Http/Controllers/API/TavsirController.php
+++ app/Http/Controllers/API/TavsirController.php
@@ -761,7 +761,7 @@ class TavsirController extends Controller
                     // if ($tenant_is_verified || $trans_order->order_type == TransOrder::ORDER_TRAVOY) {
 
                     $data = PgJmto::tarifFee($value->sof_id, $value->payment_method_id, $value->sub_merchant_id, $trans_order->sub_total);
-                 
+                    dump('x'+$data);
                     $value->percentage = $data['is_presentage'] ?? null;
                     $x = $data['value'] ?? 'x';
                     $state = $data['is_presentage'] ?? null;
