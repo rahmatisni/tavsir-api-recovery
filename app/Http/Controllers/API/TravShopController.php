e8c1c1a319473243abadf82204b24e02cbaf7b03
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-15 17:44:16 +0700

    fix status

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1674,7 +1674,7 @@ class TravShopController extends Controller
                             // return response()->json(['status' => $data->status, 'responseData' => $data->payment->data ?? '', 'kiosbank' => $kios]);
                         } else {
 
-                            Log::info(['masuk sana => ', $kios]);
+                            Log::info('masuk_sana');
                             $data->log_kiosbank()->updateOrCreate(['trans_order_id' => $data->id], [
                                 'data' => $kios,
                                 'payment' => $kios,
