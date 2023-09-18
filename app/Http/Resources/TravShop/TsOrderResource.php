7436f8e5958f42643080d7ce5d12cc37cbd4c8aa
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-18 12:21:09 +0700

    tes

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -144,7 +144,9 @@ class TsOrderResource extends JsonResource
                         // if ( $temps['data'][$key] == 'Harga_kios'){
                         //     $temps['data'][$key] = 2;
                         // }
-                        $temps['data'][$key] = $val;
+                        unset( $temps['data'][$key]);
+
+                        // $temps['data'][$key] = $val;
 
                     }
                 }
