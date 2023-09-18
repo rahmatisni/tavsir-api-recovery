dc2a9062c5c17af38addd842d6482d52d6ff0ec0
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-18 21:45:57 +0700

    ratelimit

--- app/Http/Middleware/CustomThrottleMiddleware.php
+++ app/Http/Middleware/CustomThrottleMiddleware.php
@@ -6,27 +6,18 @@ use Closure;
 use Illuminate\Http\Request;
 
 
-class CustomThrottleMiddleware extends ThrottleRequests
+
+class CustomThrottleMiddleware
 {
-    protected function resolveRequestSignature($request)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
     {
-        // Customize the request signature as needed for per-second rate limiting
-        return sha1($request->method() . '|' . $request->id());
+        return $next($request);
     }
 }
-
-
-// class CustomThrottleMiddleware
-// {
-//     /**
-//      * Handle an incoming request.
-//      *
-//      * @param  \Illuminate\Http\Request  $request
-//      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-//      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-//      */
-//     public function handle(Request $request, Closure $next)
-//     {
-//         return $next($request);
-//     }
-// }
