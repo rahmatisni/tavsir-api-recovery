406e25559bf7be910d2f7cafe3c57c083d124ceb
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-18 12:52:34 +0700

    tes

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -152,6 +152,8 @@ class TsOrderResource extends JsonResource
             }
             // if (!$temps['data']['Diskon'])
             // {
+            $temps['data']['customerID'] = '-' . rupiah((int) $this->discount);
+
             $temps['data']['Diskon'] = '-' . rupiah((int) $this->discount);
             // } 
 
