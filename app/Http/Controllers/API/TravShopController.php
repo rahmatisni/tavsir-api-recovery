817f2e4e6f11a50b2a20ad1e0dede1e4d1e9ecfd
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-09-18 22:03:15 +0700

    oke ratelimit

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1634,7 +1634,7 @@ class TravShopController extends Controller
                                     'payment' => $kios,
 
                                 ]);
-                                $data->status = TransOrder::READY;
+                                $data->status = TransOrder::PAYMENT_SUCCESS;
                                 $data->save();
                                 DB::commit();
                                 return response()->json(['status' => $data->status, 'responseData' => $data->payment->data ?? '', 'kiosbank' => $kios]);
@@ -1649,7 +1649,7 @@ class TravShopController extends Controller
                                 'payment' => $kios,
 
                             ]);
-                            $data->status = TransOrder::READY;
+                            $data->status = TransOrder::PAYMENT_SUCCESS;
                             $data->save();
                             DB::commit();
                             return response()->json(['status' => $data->status, 'responseData' => $data->payment->data ?? '', 'kiosbank' => $kios]);
