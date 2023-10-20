532b933f5b8d71faac51c928a55f6d4e442518e9
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-10-20 12:51:24 +0700

    Bagi Hasil Kalkulasi Travshop

--- app/Http/Controllers/API/TavsirController.php
+++ app/Http/Controllers/API/TavsirController.php
@@ -626,7 +626,8 @@ class TavsirController extends Controller
                 foreach ($tenant_sharing as $value) {
                     $harga = ceil($data->sub_total)+ceil($data->addon_total);
                     $sharing_amount_unround = (($value/100) * $harga);
-                    $sharing_amount[] = $sharing_amount_unround;
+                    $sharing_amount[] = number_format($sharing_amount_unround, 2, '.', '');
+
                     // dump([$value,$harga, $sharing_amount_unround, $data->sub_total,$data->addon_total]);
 
                 }
