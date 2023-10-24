d7303a53bf459567c4fc1be672cb76fd177bbb1d
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-10-24 16:20:19 +0700

    soft delete area dan ruas

--- app/Models/Ruas.php
+++ app/Models/Ruas.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use App\Models\BaseModel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Ruas extends BaseModel
 {
+    use SoftDeletes;
+
     protected $table = 'ref_ruas';
 
     protected $fillable = [
