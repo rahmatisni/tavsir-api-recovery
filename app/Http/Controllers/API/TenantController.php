e60babe868e87683e472fcfc9ad3cd5ef97757ed
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-12-04 12:47:47 +0800

    fix filter tenant

--- app/Http/Controllers/API/TenantController.php
+++ app/Http/Controllers/API/TenantController.php
@@ -33,9 +33,10 @@ class TenantController extends Controller
         $filter = $request?->filter;
         $filterLike = $request?->filterlike;
         $businessStatus = $filter['status_perusahaan'] ?? false;
-        // dd($businessStatus);
+        $intArray = array_map('intval', $filter);
 
-        $data = Tenant::with('business', 'rest_area', 'ruas', 'order', 'category_tenant')->myWheres($filter)->myWhereLikeStartCol($filterLike)
+
+        $data = Tenant::with('business', 'rest_area', 'ruas', 'order', 'category_tenant')->myWheres($intArray)->myWhereLikeStartCol($filterLike)
         ->when($businessStatus, function ($query) use ($businessStatus) {
             // Adding the filter for business.status_perusahaan
             $query->whereHas('business', function ($businessQuery) use ($businessStatus) {
