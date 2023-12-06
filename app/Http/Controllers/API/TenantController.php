8e05391ac24971df289e1b56dc68b9a8c5fd350c
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-12-06 14:50:25 +0700

    last promote

--- app/Http/Controllers/API/TenantController.php
+++ app/Http/Controllers/API/TenantController.php
@@ -32,11 +32,12 @@ class TenantController extends Controller
 
         $filter = $request?->filter;
         $filterLike = $request?->filterlike;
-        $filterAs = $request?->filteras;
+        $filterLikeas = $request?->filteras;
 
         $businessStatus = $filter['status_perusahaan'] ?? false;
        
-        $data = Tenant::with('business', 'rest_area', 'ruas', 'order', 'category_tenant')->myWheres($filter)->myWhereLikeStartCol($filterLike)->myWhereLike($filterAs)
+        $data = Tenant::with('business', 'rest_area', 'ruas', 'order', 'category_tenant')->myWheres($filter)->myWhereLikeStartCol($filterLike)
+        ->myWhereLikeCol($filterLikeas)
         ->when($businessStatus, function ($query) use ($businessStatus) {
             // Adding the filter for business.status_perusahaan
             $query->whereHas('business', function ($businessQuery) use ($businessStatus) {
