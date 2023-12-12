4e3a7802df8def92bbdf960ce65111fd19213185
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-12-12 09:28:41 +0700

    test deploy

--- routes/api.php
+++ routes/api.php
@@ -267,6 +267,7 @@ Route::middleware('customRateLimit:key,1,10')->group(function () {
     Route::get('/travshop/payment-status/{id}', [App\Http\Controllers\API\TravShopController::class, 'statusPayment']);
 });
 
+
 // Route::middleware('customRateLimit:key,10,60')->group(function () {
     Route::post('/reset-password', [App\Http\Controllers\API\UserController::class, 'resetPass']);
     Route::post('/lupa-password', [App\Http\Controllers\API\UserController::class, 'forgetPass']);
