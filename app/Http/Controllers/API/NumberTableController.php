6200cd5057915cf6267365d6d18c2af035a0f730
Rahmat Nur Isni <rahmatisni@gmail.com>
2023-12-12 10:24:19 +0700

    test deploy

--- app/Http/Controllers/API/NumberTableController.php
+++ app/Http/Controllers/API/NumberTableController.php
@@ -92,8 +92,8 @@ class NumberTableController extends Controller
         $self_order_url = env('URL_SELF_ORDER','https://getpay-selforder.jmto.co.id');
         $url = $self_order_url.'?'.http_build_query(['tenant_id' => auth()->user()->tenant_id, 'nomor' => $data->name]);
         return QrCode::size(200)
-        // ->backgroundColor(254, 200, 26)
-        // ->color(1, 55, 182)
+        ->backgroundColor(254, 200, 26)
+        ->color(1, 55, 182)
         ->margin(1)
         ->generate($url);
     }
