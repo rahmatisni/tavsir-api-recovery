94186f1e8659f840f8f36e305fc2df62f0b75a00
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-01-02 17:35:38 +0700

    supertenant order

--- app/Services/LaporanServices.php
+++ app/Services/LaporanServices.php
@@ -163,7 +163,7 @@ class LaporanServices
         $tenant_id = $request->tenant_id;
         $rest_area_id = $request->rest_area_id;
         $business_id = $request->business_id;
-        $super_tenant_id = auth()->user()->supertenant_id;
+        // $super_tenant_id = auth()->user()->supertenant_id;
 
         $data = TransOrderDetil::whereHas(
             'trans_order',
@@ -180,8 +180,8 @@ class LaporanServices
                     })->when($tenant_id, function ($qq) use ($tenant_id) {
                     return $qq->where('tenant_id', $tenant_id);
                     //  CR
-                })->when($super_tenant_id, function ($qq) use ($super_tenant_id) {
-                    return $qq->orwhere('supertenant_id', $super_tenant_id);
+                // })->when($super_tenant_id, function ($qq) use ($super_tenant_id) {
+                //     return $qq->orwhere('supertenant_id', $super_tenant_id);
                     // 
                 })->when($rest_area_id, function ($qq) use ($rest_area_id) {
                     return $qq->where('rest_area_id', $rest_area_id);
@@ -306,7 +306,7 @@ class LaporanServices
         $business_id = $request->business_id;
         $order_type = $request->order_type;
         $payment_method_id = $request->payment_method_id;
-        $super_tenant_id = auth()->user()->supertenant_id;
+        // $super_tenant_id = auth()->user()->supertenant_id;
 
         $raw_data =  $data = TransOrder::with('tenant')
         ->when(
@@ -322,19 +322,19 @@ class LaporanServices
             }
         )
         
-        // ->when($tenant_id, function ($q) use ($tenant_id) {
-        //     return $q->where('tenant_id', $tenant_id);
-        // })
+        ->when($tenant_id, function ($q) use ($tenant_id) {
+            return $q->where('tenant_id', $tenant_id);
+        })
         // CR
         
-        ->when($tenant_id, function ($qq) use ($tenant_id, $super_tenant_id) {
-            if($super_tenant_id == null){
-                return  $qq->where('tenant_id', $tenant_id);
-            }
-            else {
-                $qq->where('tenant_id', $tenant_id)->orWhere('supertenant_id', $super_tenant_id);
-            }
-        })
+        // ->when($tenant_id, function ($qq) use ($tenant_id, $super_tenant_id) {
+        //     if($super_tenant_id == null){
+        //         return  $qq->where('tenant_id', $tenant_id);
+        //     }
+        //     else {
+        //         $qq->where('tenant_id', $tenant_id)->orWhere('supertenant_id', $super_tenant_id);
+        //     }
+        // })
         // CR
         ->when($rest_area_id, function ($qq) use ($rest_area_id) {
         return $qq->where('rest_area_id', $rest_area_id);
