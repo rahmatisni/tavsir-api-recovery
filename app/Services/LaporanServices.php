9f874cbf8c59c7ee9634122bb8dadf3c84863c97
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-01-08 08:38:00 +0700

    Fix laporan penjualan service

--- app/Services/LaporanServices.php
+++ app/Services/LaporanServices.php
@@ -221,11 +221,19 @@ class LaporanServices
                 // });
             }
         )
-            ->whereHas('product', function ($qq) use ($tenant_id, $super_tenant_id) {
+            ->whereHas('product', function ($qq) use ($tenant_id, $super_tenant_id, $rest_area_id, $business_id) {
                 if (auth()->user()->tenant->is_supertenant != null) {
                     if ($tenant_id != null) {
                         $qq->where('tenant_id', $tenant_id);
                     }
+                } elseif (auth()->user()->role != 'TENANT') {
+                    $qq->when($tenant_id, function ($qq) use ($tenant_id) {
+                        return $qq->where('tenant_id', $tenant_id);
+                    })->when($rest_area_id, function ($qq) use ($rest_area_id) {
+                        return $qq->where('rest_area_id', $rest_area_id);
+                    })->when($business_id, function ($qq) use ($business_id) {
+                        return $qq->where('business_id', $business_id);
+                    });
                 } else {
                     $qq->where('tenant_id', auth()->user()->tenant->id);
                 }
