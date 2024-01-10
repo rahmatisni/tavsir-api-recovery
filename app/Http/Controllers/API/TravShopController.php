1170b74040c389e4ce054ce80f6bff4cdfc38e74
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-01-10 17:05:51 +0700

    fix member-tenant

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -78,7 +78,6 @@ class TravShopController extends Controller
         });
         $data = $data->get();
 
-
         if ($request->lat && $request->lon) {
             $data = $data->filter(function ($item) use ($request) {
                 return $this->haversine($item->latitude, $item->longitude, $request->lat, $request->lon, $request->distance ?? 1);
