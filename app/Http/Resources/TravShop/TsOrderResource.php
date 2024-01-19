7bfbc04485998b34f7baa335e23f0f1a12be2dd3
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-01-19 09:53:03 +0700

    add detil on pln jatelindo

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -199,7 +199,7 @@ class TsOrderResource extends JsonResource
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'paid_date' => $this->payment->updated_at ?? null,
             'payment' => $this->payment->data ?? null,
-            'log_kiosbank' => $temps ?? $this->log_kiosbank,
+            'log_kiosbank' => $product_kios_bank->integrator == 'JATELINDO' ? ['data'=> $product_kios]:($temps ?? $this->log_kiosbank),
             'addon_total' => $this->addon_total,
             'addon_price' => $this->addon_price,
             'detil_kios' => $product_kios,
