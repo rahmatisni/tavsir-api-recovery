c22245b57954e698e30182462ed7c12e1aa458fc
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-13 17:11:34 +0700

    unset name

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -68,7 +68,7 @@ class TsOrderResource extends JsonResource
                         $product_kios['Daya'] = str_pad($daya, 9, "0", STR_PAD_LEFT);
                         unset($product_kios['Ref_ID']);
                         $product_kios['Nama_Produk'] = $product_kios['name'];
-                        unset($product_kios['name']);
+                        // unset($product_kios['name']);
                         unset($product_kios['logo_url']);
                         unset($product_kios['Transaksi_ID']);
                         unset($product_kios['Vending_Number']);
