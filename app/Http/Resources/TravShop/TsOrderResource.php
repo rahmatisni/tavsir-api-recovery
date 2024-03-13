60a5e681526d94b692c6e31caff55e17136344ed
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-13 15:48:24 +0700

    Jatelindo Format PLN

--- app/Http/Resources/TravShop/TsOrderResource.php
+++ app/Http/Resources/TravShop/TsOrderResource.php
@@ -63,6 +63,11 @@ class TsOrderResource extends JsonResource
                     
                     $product_kios = array_merge($product_kios, JatelindoService::infoPelanggan($this->log_kiosbank, $this->status));
                     // if ($this->status === TransOrder::WAITING_PAYMENT) {
+
+                        $daya = $product_kios['Daya'];
+                        $product_kios['Daya'] = str_pad($daya, 9, "0", STR_PAD_LEFT);
+                        unset($product_kios['name']);
+                        unset($product_kios['Ref_ID']);
                         unset($product_kios['Transaksi_ID']);
                         unset($product_kios['Vending_Number']);
                         unset($product_kios['Informasi']);
@@ -73,6 +78,8 @@ class TsOrderResource extends JsonResource
                         unset($product_kios['Pilihan_Token']);
                         unset($product_kios['Token_Unsold_1']);
                         unset($product_kios['Token_Unsold_2']);
+                        dd($product_kios);
+
                     // }
                     // if ($this->status === TransOrder::DONE) {
                     //     unset($product_kios['flag']);
