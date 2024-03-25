76d98fb90eeca8b17374602b32b47584e99a7116
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-25 11:33:44 +0700

    fux

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -3253,7 +3253,7 @@ class TravShopController extends Controller
 
     public function callbackpg(Request $request)
     {
-        log::info(['returncallbackPG =>', $request]);
+        log::info(['returncallbackPG =>', $request->all()]);
         return response()->json(['status' => 'success', 'data' => 'Data di Terima'], 200);
     }
 
