fd49addb4140b7d5f70670ed89736e434ecfedd7
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-04-02 14:45:51 +0700

    tambah all

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -912,7 +912,7 @@ class TravShopController extends Controller
             $param_removes = Tenant::where('id', $trans_order->tenant_id)->first();
             if ($param_removes == null && $trans_order->order_type == 'ORDER_TRAVOY') {
                 $self_order = [];
-                $travshop = [5, 6, 7, 8, 9, 11];
+                $travshop = [5, 6, 7, 8, 9, 11,12,13];
                 // $travshop = [5, 6, 7, 8, 9];
                 $tavsir = [];
 
