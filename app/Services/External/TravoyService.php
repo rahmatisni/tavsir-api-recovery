fe4dbf1247fc7a158543eeceedaa3efc3976325e
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-05-02 10:34:43 +0700

    Add tracehold exp_date

--- app/Services/External/TravoyService.php
+++ app/Services/External/TravoyService.php
@@ -78,7 +78,7 @@ class TravoyService
             
         ];
         $res_json = $this->http('POST', self::detilHU, $payLoad);
-        log::info('callbackHU', $res_json->json());
+        log::info('callbackHU');
         return $res_json;
 
     }
