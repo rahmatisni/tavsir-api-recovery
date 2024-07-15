4ad649909b0f7b362b2cc94bf73aecdedcd0e41f
rahmatisni@gmail.com <rahmatisni@gmail.com>
2024-07-15 10:03:11 +0700

    test pg

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1081,7 +1081,7 @@ class TravShopController extends Controller
                         $data = PgJmto::tarifFee($value->sof_id, $value->payment_method_id, $trans_order->sub_merchant_id, $trans_order->sub_total);
                         // Log::warning($data);
                         // dump($data);
-                        // dump([$value->sof_id, $value->payment_method_id, $trans_order->sub_merchant_id, $trans_order->sub_total]);
+                        dump([$value->code_name, $value->sof_id, $value->payment_method_id, $trans_order->sub_merchant_id, $trans_order->sub_total]);
                         $value->percentage = $data['is_presentage'] ?? null;
                         $x = $data['value'] ?? 'x';
                         $state = $data['is_presentage'] ?? null;
