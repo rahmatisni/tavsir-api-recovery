a7a35e1673663161fafde448471c2c876cf0cc31
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-07-22 14:02:36 +0700

    add self biaya admin bank

--- app/Services/External/JatelindoService.php
+++ app/Services/External/JatelindoService.php
@@ -475,7 +475,8 @@ class JatelindoService
                 'Total_Token_Unsold' => $total_token_unsold,
                 'Token_Unsold_1' => number_format((int) $harga_token_unsold_1, 0, ',', '.'),
                 'Token_Unsold_2' => number_format((int) $harga_token_unsold_2, 0, ',', '.'),
-                'Admin_Bank' => $biaya_admin === 0 ? 0 : 'Rp. ' . number_format((float) $biaya_admin, 0, ',', '.'),
+                // 'Admin_Bank' => $biaya_admin === 0 ? 0 : 'Rp. ' . number_format((float) $biaya_admin, 0, ',', '.'),
+                'Admin_Bank' => $biaya_admin === 0 ? 0 : $biaya_admin
 
             ];
         }
@@ -496,7 +497,7 @@ class JatelindoService
             // 'JML_KWH' => number_format((float)$biaya_kwh / 100, $digit_kwh, ',', '.'),
             'JML_KWH' => number_format((float)$biaya_kwh / 100, 1, ',', '.'),
             'STROOM/TOKEN' => wordwrap($token, 4, ' ', true),
-            'Admin_Bank' => $biaya_admin === 0 ? 0 : 'Rp. ' . number_format((float) $biaya_admin, 0, ',', '.'),
+            'Admin_Bank' => $biaya_admin === 0 ? 0 : $biaya_admin,
             'Informasi' => $bit_62,
             'KETERANGAN' => self::responseTranslation(($log->payment ?? $log->inquiry))?->message
 
