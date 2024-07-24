7fd5dffe1055dcbed6d0d564b76df83a27aa1e47
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-07-24 13:25:19 +0700

    fix bug paykios

--- app/Services/Payment/PaymentService.php
+++ app/Services/Payment/PaymentService.php
@@ -805,7 +805,6 @@ class PaymentService
                     'repate_count' => $datalog->data['repeate_count'] ?? $repeate_count,
                 ]);
             }
-            dd($datalog);
             $tagihan = $datalog['data']['tagihan'] ?? $datalog['data']['harga_kios'];
             $admin = $datalog['data']['adminBank'] ?? $datalog['data']['AB'] ?? '000000000000';
             $total = $datalog['data']['total'] ?? $datalog['data']['harga_kios'] ?? $tagihan;
