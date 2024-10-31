edf7ab22947189e62eb98da4a007d0825c179cb9
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-10-31 09:22:36 +0700

    add qris jtl

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1068,7 +1068,7 @@ class TravShopController extends Controller
             $param_removes = Tenant::where('id', $trans_order->tenant_id)->first();
             if ($param_removes == null && $trans_order->order_type == 'ORDER_TRAVOY') {
                 $self_order = [];
-                $travshop = [5, 6, 7, 8, 9, 11, 12, 13, 14];
+                $travshop = [4,5, 6, 7, 8, 9, 11, 12, 13, 14, 15];
                 $tavsir = [];
 
                 // } else if ($param_removes == null && $trans_order->order_type == 'HU_ORDER') {
