0159f70647dcaaac58da1dda7a3e9ccef39485ff
Rahmat Nur Isni <rahmatisni@gmail.com>
2025-03-07 10:20:40 +0700

    shotout payment method

--- app/Http/Controllers/API/TravShopController.php
+++ app/Http/Controllers/API/TravShopController.php
@@ -1215,11 +1215,6 @@ class TravShopController extends Controller
                     } else {
                         $value->fee = (int) ceil((float) $x / 100 * $trans_order->sub_total);
                     }
-                    // DUMMY
-                    $value->fee = (int)PaymentMethod::where('id', $value->id)->pluck('service_fee')->first();
-                    if($value->fee == 0){
-                        $value->travshop = false;
-                    }
                 }
 
                 if ($value->id == 4) {
